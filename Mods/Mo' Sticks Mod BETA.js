906da897304786a11e90aa4a84f68f74a1d1a1aa
ModPE <modpedragonsteve142@gmail.com>
2015-06-16 18:39:09 +0800

    Update Mo' Sticks Mod BETA.js

--- Mods/Mo' Sticks Mod BETA.js
+++ Mods/Mo' Sticks Mod BETA.js
@@ -2,15 +2,6 @@
 
 ModPE.setItem(500,"stick",0,"Victim Flinging Stick ");
 Item.addCraftRecipe(500, 1, 0, [280, 1, 0, 288, 2, 0]);
-function attackHook(attacker,victim){
-
-  if(getCarriedItem()==500){
-
-    setVelY(victim, 2);
-    preventDefault();
-
-}
-}
 
 ModPE.setItem(501,"stick",0,"Flaming Stick ");
 Item.addCraftRecipe(501, 1, 0, [280, 1, 0, 318, 2, 0]);
@@ -18,7 +9,13 @@ function attackHook(attacker,victim){
 
   if(getCarriedItem()==501){
 
-    Entity.setFireTicks(victim, 200);
+    Entity.setFireTicks(entity, 200);
+  }
+    
+    else if(getCarriedItem()==500){
+
+    setVelY(victim, 2);
+    preventDefault();
 
 }
 }
