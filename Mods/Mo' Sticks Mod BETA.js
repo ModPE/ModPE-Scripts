3f64abe2cf5938614a943d15798ee31d9b0a05aa
ModPE <modpedragonsteve142@gmail.com>
2015-06-16 19:27:48 +0800

    Update Mo' Sticks Mod BETA.js

--- Mods/Mo' Sticks Mod BETA.js
+++ Mods/Mo' Sticks Mod BETA.js
@@ -1,23 +1,18 @@
 //Mo' Sticks Mod by DragonSteve142(ModPE)
 
 ModPE.setItem(500,"stick",0,"Victim Flinging Stick ");
-Item.addCraftRecipe(500, 1, 0, [280, 1, 0, 288, 2, 0]);
-
 ModPE.setItem(501,"stick",0,"Flaming Stick ");
-Item.addCraftRecipe(501, 1, 0, [280, 1, 0, 318, 2, 0]);
-
 ModPE.setItem(502,"stick",0,"Vampiric Stick ");
-Item.addCraftRecipe(502, 1, 0, [280, 1, 0, 291, 1, 0]);
-
 ModPE.setItem(503,"stick",0,"Invisibility Stick ");
-Item.addCraftRecipe(503, 1, 0, [280, 1, 0, 20, 2, 0]);
-
 ModPE.setItem(504,"stick",0,"Riding Stick ");
-Item.addCraftRecipe(504, 1, 0, [280, 1, 0, 352, 2, 0]);
-
 ModPE.setItem(505,"stick",0,"Support Stick ");
-Item.addCraftRecipe(505, 1, 0, [280, 1, 0, 297, 2, 0]);
 
+Item.addCraftRecipe(505, 1, 0, [280, 1, 0, 297, 2, 0]);
+Item.addCraftRecipe(500, 1, 0, [280, 1, 0, 288, 2, 0]);
+Item.addCraftRecipe(504, 1, 0, [280, 1, 0, 352, 2, 0]);
+Item.addCraftRecipe(503, 1, 0, [280, 1, 0, 20, 2, 0]);
+Item.addCraftRecipe(502, 1, 0, [280, 1, 0, 291, 1, 0]);
+Item.addCraftRecipe(501, 1, 0, [280, 1, 0, 318, 2, 0]);
 
 function useItem(x,y,z,itemId,blockId,side){
 
@@ -51,7 +46,7 @@ function attackHook(attacker,victim){
 
     else if(getCarriedItem()==505){
 
-    Entity.setHealth(victim, Entity.getHealth(victim) + 50);
+    Entity.setHealth(victim, Entity.getHealth(victim) + 100);
     }
 
     else if(getCarriedItem()==504){
