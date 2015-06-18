a669d0bf336c145f0bffd47baa5705b96bb43ee6
ModPE <modpedragonsteve142@gmail.com>
2015-06-18 08:33:31 +0800

    Create SIB Mod.js

---
+++ Mods/SIB Mod.js
@@ -0,0 +1,30 @@
+//Secret Items & Blocks Mod by DragonSteve142(ModPE)
+Block.defineBlock(137, "Command Block", [["command_block", 0], ["command_block", 0], ["command_block", 0], ["command_block", 0], ["command_block", 0], ["command_block", 0]]);
+Block.defineBlock(95, "Invisible Bedrock", [[air, 0], [air, 0], [air, 0], [air, 0], [air, 0], [air, 0]]);
+
+Player.addItemCreativeInv(137,1,0);
+Player.addItemCreativeInv(95,1,0);
+Player.addItemCreativeInv(378,1,0);
+Player.addItemCreativeInv(8,1,0);
+Player.addItemCreativeInv(9,1,0);
+Player.addItemCreativeInv(10,1,0);
+Player.addItemCreativeInv(11,1,0);
+Player.addItemCreativeInv(31,1,0);
+Player.addItemCreativeInv(51,1,0);
+Player.addItemCreativeInv(60,1,0);
+Player.addItemCreativeInv(62,1,0);
+Player.addItemCreativeInv(127,1,0);
+Player.addItemCreativeInv(141,1,0);
+Player.addItemCreativeInv(142,1,0);
+Player.addItemCreativeInv(198,1,0);
+Player.addItemCreativeInv(244,1,0);
+Player.addItemCreativeInv(246,1,0);
+Player.addItemCreativeInv(248,1,0);
+Player.addItemCreativeInv(249,1,0);
+Player.addItemCreativeInv(255,1,0);
+Player.addItemCreativeInv(302,1,0);
+Player.addItemCreativeInv(303,1,0);
+Player.addItemCreativeInv(304,1,0);
+Player.addItemCreativeInv(305,1,0);
+Player.addItemCreativeInv(330,1,0);
+Player.addItemCreativeInv(341,1,0);
